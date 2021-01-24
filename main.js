d49c2527379e6b70905a5e9d7c5f69d2c12e1621
Nick <kuznaik@gmail.com>
2021-01-24 15:50:16 -0600

    0.3.2 improved mobile support
    todo: affix sidebar on mobile
    todo: disable doubletap zoom on mobile
    todo: disable scroll on mobile

--- main.js
+++ main.js
@@ -3,55 +3,77 @@ const mapImg = document.getElementById('map__img')
 const menuMoves = document.getElementById('menu__moves')
 
 
+// zoom functionality
+let scale = 1;
 function zoom(event) {
-    scale += event.deltaY * -0.001;
+    scale += event.deltaY * -0.001
     // Restrict scale
-    scale = Math.min(Math.max(0.5, scale), 4);
+    scale = Math.min(Math.max(0.5, scale), 4)
     // Apply scale transform
-    map.style.transform = `scale(${scale})`;
+    map.style.transform = `scale(${scale})`
   }
-  let scale = 1;
 
 
-var img_ele = null,
-  x_cursor = 0,
-  y_cursor = 0,
-  x_img_ele = 0,
-  y_img_ele = 0;
+// Pan Functionality
+let img_ele = null
+let x_cursor = 0
+let y_cursor = 0
+let x_img_ele = 0
+let y_img_ele = 0
 
 function start_drag() {
     img_ele = this;
-    x_img_ele = window.event.clientX - mapImg.offsetLeft;
-    y_img_ele = window.event.clientY - mapImg.offsetTop;
-
+    console.log(img_ele)
+    if (event.type == 'mousedown') {
+        x_img_ele = window.event.clientX - mapImg.offsetLeft
+        y_img_ele = window.event.clientY - mapImg.offsetTop
+    } else if (event.type == 'touchstart') {
+        let touch = event.touches[0] || event.changedTouches[0]
+        x_img_ele = touch.clientX - mapImg.offsetLeft
+        y_img_ele = touch.clientY - mapImg.offsetTop
+        console.log('touchstart',x,y)
+    }
 }
 
 function stop_drag() {
-    img_ele = null;
+    img_ele = null
 }
 
 function while_drag() {
-    var x_cursor = window.event.clientX;
-    var y_cursor = window.event.clientY;
+    console.log(arguments)
+    if (event.type == 'mousemove') {
+        x_cursor = window.event.clientX
+        y_cursor = window.event.clientY
+    } else if (event.type = 'touchmove') {
+        let touch = event.touches[0] || event.changedTouches[0]
+        x_cursor = touch.clientX
+        y_cursor = touch.clientY
+    }
     if (img_ele !== null) {
-        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
-        img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
+        img_ele.style.left = (x_cursor - x_img_ele) + 'px'
+        img_ele.style.top = ( y_cursor - y_img_ele) + 'px'
         //track movement in console
-        //console.log(img_ele.style.left+' - '+img_ele.style.top);
+        console.log(img_ele.style.left+' - '+img_ele.style.top)
 
     }
 }
   
 // enable pan
-mapImg.addEventListener('mousedown', start_drag);
-map.addEventListener('mousemove', while_drag);
-map.addEventListener('mouseup', stop_drag);
+mapImg.addEventListener('mousedown', start_drag)
+map.addEventListener('mousemove', while_drag)
+map.addEventListener('mouseup', stop_drag)
+mapImg.addEventListener('touchstart', start_drag)
+map.addEventListener('touchmove', while_drag)
+map.addEventListener('touchend', stop_drag)
 // enable zoom
-map.onwheel = zoom;
+map.onwheel = zoom
 // disable right-click the image
-mapImg.addEventListener('contextmenu', event => event.preventDefault());
+mapImg.addEventListener('contextmenu', event => event.preventDefault())
 
 
+
+
+// UI - Buttons
 let moveID = 1
 let moveIDtoURL = {
     13: 'images/karta_13_khod.png',
@@ -78,8 +100,8 @@ function updateURL(event) {
             mapImg.src = newURL
             document.getElementsByClassName('button active')[0].classList.remove('active')
             event.target.classList.add('active')
-            event.stopPropagation();
-            event.preventDefault();
+            event.stopPropagation()
+            event.preventDefault()
         } else {
             mapImg.src = ''
         } 
